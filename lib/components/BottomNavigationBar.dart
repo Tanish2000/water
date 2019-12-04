3a2e1cbbcf305940d2cee12931204fd41f57aa67
girishra <chaudharygirish7@gmail.com>
2019-12-04 20:07:30 +0530

    detail

--- lib/components/BottomNavigationBar.dart
+++ lib/components/BottomNavigationBar.dart
@@ -1,43 +1,31 @@
 import 'package:flutter/material.dart';
-
 import 'package:water/Screens/Dashboard.dart';
-
 import 'package:water/Screens/Cart.dart';
 import 'package:water/Screens/Profile.dart';
-
-
-
-
-
 class bottomNavigationBar extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _BottomNavigationBarState();
   }
 }
-
 class _BottomNavigationBarState extends State<bottomNavigationBar > {
   int _selectedPage = 0;
   final _pageOptions = [
     Dashboard(),
     Cart(),
     Profile()
-
   ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pageOptions[_selectedPage],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedPage,
-
         onTap: (int index){
           setState(() {
             _selectedPage = index;
           });
         },
-
         items: [
       BottomNavigationBarItem(
         icon: Icon(Icons.dashboard),title: Text("Dashboard")
@@ -48,12 +36,10 @@ class _BottomNavigationBarState extends State<bottomNavigationBar > {
           BottomNavigationBarItem(
               icon: Icon(Icons.person),title: Text("Profile")
           ),
-
         ],
       ),
     );
   }
-
   void onTabTapped(int index) {
     setState(() {
       _selectedPage = index;
